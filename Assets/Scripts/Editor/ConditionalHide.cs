ad0c17beff14b1682dd452a73e3edbf7de7cf2fa
Fabrício <fabricio-gf@hotmail.com>
2019-03-23 00:05:49 -0300

    Added function adding to buttons and updated gameover and victory

---
+++ Assets/Scripts/Editor/ConditionalHide.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(AddLoadBehaviourToButton))]
+public class ConditionalHide : Editor
+{
+    override public void OnInspectorGUI()
+    {
+        var addLoadBehaviourToButton = target as AddLoadBehaviourToButton;
+
+        EditorGUILayout.HelpBox("Only one of the options can be selected (Restriction pending)", MessageType.Info);
+
+        addLoadBehaviourToButton.AddRestartScene = EditorGUILayout.Toggle("Add Restart Scene", addLoadBehaviourToButton.AddRestartScene);
+
+        addLoadBehaviourToButton.AddLoadScene = EditorGUILayout.Toggle("Add Load Scene", addLoadBehaviourToButton.AddLoadScene);
+
+        if (addLoadBehaviourToButton.AddLoadScene)
+        {
+            addLoadBehaviourToButton.SceneName = EditorGUILayout.TextField("Scene Name", addLoadBehaviourToButton.SceneName);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Testing/TestVictory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVictory : MonoBehaviour
+{
+    public KeyCode TestKey;
+    TriggerVictory Script;
+
+    private void Awake()
+    {
+        Script = GetComponent<TriggerVictory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(TestKey))
+        {
+            Script.Victory();
+        }
+    }
+}
