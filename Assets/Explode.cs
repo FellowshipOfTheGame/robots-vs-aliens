942b1e60ca362473045b6b9564ae0308d87bc20b
Tiago M. Silva <tmsilva11@gmail.com>
2019-04-11 18:07:47 -0300

    Robo Cherry
    Robô que explode ao ser colocado na fase.

---
+++ Assets/Explode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    private Cooldown myCooldown;
+    [SerializeField] private Transform _Dynamic;
+
+    private void Start()
+    {
+        myCooldown = gameObject.GetComponent<Cooldown>();
+        myCooldown.OnCooldownEnded += ExplodeObject;
+        _Dynamic = GameObject.FindGameObjectWithTag("Dynamic").transform;
+    }
+
+    public void ExplodeObject()
+    {
+        gameObject.GetComponent<SpawnObject>().Spawn(transform.position, Quaternion.identity, _Dynamic);
+        Debug.Log("Exploding...");
+    }
+}
