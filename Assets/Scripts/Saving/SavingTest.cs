96190a90f0a62f89bb02dae59484af885798d6c7
Fabrício <fabricio-gf@hotmail.com>
2019-04-19 15:04:11 -0300

    Finished base of save/load system

---
+++ Assets/Scripts/Saving/SavingTest.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SavingTest : MonoBehaviour
+{
+    int progress = 0;
+    public Text progressText;
+
+    // MONOBEHAVIOUR METHODS
+
+    private void Start()
+    {
+        LoadProgress();
+    }
+
+    private void OnEnable()
+    {
+        SaveData.OnBeforeSave += ApplyData;
+        SaveData.OnLoad += LoadData;
+    }
+
+    private void OnDisable()
+    {
+        SaveData.OnBeforeSave -= ApplyData;
+        SaveData.OnLoad -= LoadData;
+    }
+
+    // DELEGATE METHODS
+
+    public void ApplyData()
+    {
+        SaveData.AddProgressData(progress);
+    }
+
+    public void LoadData()
+    {
+        progress = SaveData._data.Progress;
+        progressText.text = "Current progress: " + progress;
+    }
+
+    // BUTTON CALL METHODS
+
+    public void AddProgress()
+    {
+        progress++;
+        progressText.text = "Current progress: " + progress;
+    }
+
+    public void SaveProgress()
+    {
+        SaveData.Save(SaveData._data);
+    }
+
+    public void LoadProgress()
+    {
+        SaveData.Load();
+    }
+
+    public void ResetSave()
+    {
+        progress = 0;
+        SaveData.ResetSave();
+        LoadData();
+    }
+}
