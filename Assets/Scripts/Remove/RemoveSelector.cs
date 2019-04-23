a27fffce154fa15db4b18443884b654f36f5fc92
Tiago M. Silva <tmsilva11@gmail.com>
2019-04-23 17:44:52 -0300

    Remove Button
    MouseClicks Event added

---
+++ Assets/Scripts/Remove/RemoveSelector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveSelector : MonoBehaviour
+{
+    private int RemovedIndex = 0;
+    private int RemovedCost = 0;
+    private bool RemoveSelected = false;
+
+    [SerializeField] private MouseEvents MouseScript;
+
+    private void Awake()
+    {
+        if (MouseScript == null) MouseScript = FindObjectOfType<MouseEvents>();
+        MouseScript.MouseRightClick += SelectedOff;
+    }
+
+    public bool isSelected()
+    {
+        return RemoveSelected;
+    }
+
+    public void SelectedOn()
+    {
+        RemoveSelected = true;
+    }
+
+    public void SelectedOff()
+    {
+        RemoveSelected = false;
+    }
+
+    public void SwitchSelected()
+    {
+        RemoveSelected = !RemoveSelected;
+    }
+
+}
