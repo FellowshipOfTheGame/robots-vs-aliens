3d8da8fdb38f618d94d0cb0b4d7d2181cf300fad
Tiago M. Silva <tmsilva11@gmail.com>
2019-04-23 22:00:18 -0300

    Added Animations
    Cat and Dog idle and action animations

---
+++ Assets/Scripts/Core/Animation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    private Animator animator;
+    public float animationTime = 0;
+    
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayTriggerAnimation(string name)
+    {
+        animator.SetTrigger(name);
+    }
+}
