1878454fef6daa2abbaedf8b224f4f4707b3ab36
Tiago M. Silva <tmsilva11@gmail.com>
2019-06-05 21:54:01 -0300

    Robots only shoot with alien in front
    Changed attacks and animations to use Cooldown script and added ray casting for target detection.

--- Assets/Scripts/Core/RobotBehavior.cs
+++ Assets/Scripts/Core/RobotBehavior.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class RobotBehavior : MonoBehaviour
 {
-    private Attack myAttack;
-    private Animation animationScript;
-    private CellOccupation occupiedCell;
+    private Attack myAttack = null;
+    private Animation animationScript = null;
+    private CellOccupation occupiedCell = null;
+    private BoxCollider2D myCollider = null;
+
+    private bool rayCollision = false;
+
+    [SerializeField] private Cooldown attackCooldown = null;
+    [SerializeField] private Cooldown animationCooldown = null;
+
+
 
     private void Awake()
     {
         myAttack = gameObject.GetComponent<Attack>();
         animationScript = GetComponent<Animation>();
+        myCollider = GetComponent<BoxCollider2D>();
         occupiedCell = transform.parent.GetComponent<CellOccupation>();
         if(myAttack != null)
         {
-            InvokeRepeating("PeriodicAttack", myAttack._attackSpeed, myAttack._attackSpeed); //Start attacking
-            InvokeRepeating("AttackAnimation", myAttack._attackSpeed-animationScript.animationTime, myAttack._attackSpeed); //Start attacking animation
+            attackCooldown.CooldownTime = animationScript.animationTime;
+            animationCooldown.CooldownTime = myAttack._attackSpeed;
+
+            attackCooldown.OnCooldownEnded += PeriodicAttack;
+            animationCooldown.OnCooldownEnded += AttackAnimation;
         }
         gameObject.GetComponent<CollisionControl>().OnCollision += BeingAttacked;
         gameObject.GetComponent<Life>().OnDeath += Death;
@@ -36,12 +48,21 @@ public class RobotBehavior : MonoBehaviour
     //For doing his attacks
     private void PeriodicAttack()
     {
-        myAttack.ReleaseAttack();
+        if (rayCollision)
+        {
+            myAttack.ReleaseAttack();
+        }
     }
 
     private void AttackAnimation()
     {
-        animationScript.PlayTriggerAnimation("Attack");
+        rayCollision = RayCollision();
+        if (rayCollision)
+        {
+            animationScript.PlayTriggerAnimation("Attack");
+            animationCooldown.ResetCooldown();
+            attackCooldown.ResetCooldown();
+        }
     }
 
     //Destroys Object when dead
@@ -49,6 +70,22 @@ public class RobotBehavior : MonoBehaviour
     {
         occupiedCell.UpdateCellOccupation(false);
         gameObject.GetComponent<DestroyObject>().DestroySelf();
+    }
+
+    bool RayCollision()
+    {
+        /*Physics2D.queriesStartInColliders = false;
+        ContactFilter2D thisfilter = new ContactFilter2D();
+        thisfilter.layerMask = LayerMask.GetMask("Alien"); */
+        RaycastHit2D[] hitInfo = new RaycastHit2D[11];
+        //Physics2D.Raycast(transform.position, transform.right, thisfilter, hitInfo);
+        Physics2D.Raycast(transform.position, transform.right, new ContactFilter2D(), hitInfo);
+
+        foreach (RaycastHit2D hit in hitInfo){
+            if (hit.collider != null)
+                if(hit.collider.CompareTag("Alien")) return true;
+        }
+        return false;
     }
 
 }
